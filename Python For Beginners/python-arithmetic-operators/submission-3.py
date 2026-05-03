77e630db0ee93408eee9ab4a634537bf99943dc8
yesitgoes <164319821+yesitgoes@users.noreply.github.com>
2026-05-04 04:51:55 +0900

    Add: python-arithmetic-operators - submission-3

---
+++ Python For Beginners/python-arithmetic-operators/submission-3.py
@@ -0,0 +1,11 @@
+a, b, c = 2, 2, 0.5
+total_sum = a + b + c
+minus_sum = 0 - total_sum
+total_product = a * b * c
+result = total_sum / total_product
+print(total_sum)
+print(minus_sum)
+print(total_product)
+print(result)
+
+
