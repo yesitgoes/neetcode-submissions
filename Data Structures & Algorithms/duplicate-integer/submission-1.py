99af3b2476daaff83384fa1420c8f78a9c00c4fd
yesitgoes <164319821+yesitgoes@users.noreply.github.com>
2026-08-12 15:14:43 +0900

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hashmap = {}
+        for num in nums:
+            if num not in hashmap:
+                hashmap[num] = 1
+            else:
+                return True
+        return False
